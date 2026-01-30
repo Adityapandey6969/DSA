464e823f11dd9e000dbd3f98f2755e971ed019f9
Adityapandey6969 <adityapandey2479@gmail.com>
2026-01-30 22:10:34 +0530

    Time: 376 ms (29.94%) | Memory: 43.2 MB (12.91%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        backtrack(board);
+    }
+
+    private boolean backtrack(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (backtrack(board)) {
+                                return true;
+                            }
+
+                            board[row][col] = '.'; // backtrack
+                        }
+                    }
+                    return false; // no valid number found
+                }
+            }
+        }
+        return true; // solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[row][i] == num) return false;
+
+            // Check column
+            if (board[i][col] == num) return false;
+
+            // Check 3x3 sub-box
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+            if (board[boxRow][boxCol] == num) return false;
+        }
+        return true;
+    }
+}
+ 
