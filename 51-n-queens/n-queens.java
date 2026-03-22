d8cc2bf196c113cab70936944dad48f9cb3a70fb
Adityapandey6969 <adityapandey2479@gmail.com>
2026-03-23 01:52:21 +0530

    Time: 2 ms (82.23%) | Memory: 46.7 MB (57.95%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (char[] row : board) Arrays.fill(row, '.');
+        backtrack(board, 0, n, result);
+        return result;
+    }
+    
+    private void backtrack(char[][] board, int row, int n, List<List<String>> result) {
+        if (row == n) {
+            List<String> solution = new ArrayList<>();
+            for (char[] r : board) solution.add(new String(r));
+            result.add(solution);
+            return;
+        }
+        
+        for (int col = 0; col < n; col++) {
+            if (isValid(board, row, col, n)) {
+                board[row][col] = 'Q';
+                backtrack(board, row + 1, n, result);
+                board[row][col] = '.';
+            }
+        }
+    }
+    
+    private boolean isValid(char[][] board, int row, int col, int n) {
+        // Check column
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 'Q') return false;
+        
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q') return false;
+        
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q') return false;
+        
+        return true;
+    }
+}
